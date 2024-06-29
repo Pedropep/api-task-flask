180b9f04340b028d8ca9e1cfac07232f632cf6a1
Pedro E <pedroestevaopep@gmail.com>
2024-06-29 19:26:25 -0300

    Criando teste das requisições POST e GET

---
+++ pyteste.py
@@ -0,0 +1,35 @@
+import pytest
+import requests
+
+#CRUD
+BASE_URL = "http://127.0.0.1:5000"
+tarefa = []
+
+def teste_create_tarefa():
+    nova_tarefa_data = {
+        "titulo": "Nova Tarefa",
+        "descricao":"Descrição da nova tarefa"
+    }
+
+    response = requests.post(f"{BASE_URL}/task", json=nova_tarefa_data)
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "message" in response_json
+    assert "id" in response_json
+    tarefa.append(response_json['id'])
+
+def teste_get_allTarefas():
+
+    reponse = requests.get(f"{BASE_URL}/task")
+    assert reponse.status_code == 200
+    reponse_json = reponse.json()
+    assert "task" in reponse_json
+    assert "total_task" in reponse_json
+
+def teste_get_byId():
+    if tarefa: 
+        tarefa_id = tarefa[0]
+        response = requests.get(f"{BASE_URL}/task/{tarefa_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert tarefa_id == response_json['id']
